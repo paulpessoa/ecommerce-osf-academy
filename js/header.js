dd3e53867c5f9416f1b59b64d841d1eee2ed6802
Paul Pessoa <paulmspessoa@gmail.com>
2021-06-11 13:02:45 -0300

    toggle dropdown

--- js/header.js
+++ js/header.js
@@ -1,17 +1,9 @@
-$(document).ready(function() {
-    $('i').click (
-        function(){
-            $('div.d-menu-content').slideDown(500);
-        },
-            function(){
-            $('div.d-menu-content').slideUp(500);
-            }
-    );
-});
-
 $(document).ready(function(){
     $("i").click(function(){
-        $('div.d-menu-content').slideDown(500);
+        $('.d-menu-content').slideToggle("slow");
+     
     }
     );
   });
+
+

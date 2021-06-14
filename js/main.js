6c71cd0211a0ea4237d933eec50a5321210677cd
Paul Pessoa <paulmspessoa@gmail.com>
2021-06-14 15:27:14 -0300

    function js add to cart and add to favorite list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 
+// add one more item to favorite list
+numFav = 0;  
+function addFav() {
+    numFav++;
+    document.getElementById('add-favorite-sum').textContent = numFav;
+    }  
+
+// add one more item to cart
+numCart = 0;
+function addCart() {
+    numCart++;
+    document.getElementById('add-cart-sum').textContent = numCart;
+    }
+    

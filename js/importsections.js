04a2383e6ea12c4397bb00fbf69b3f72665a50fc
Paul Pessoa <paulmspessoa@gmail.com>
2021-06-13 21:56:30 -0300

    popular items style

--- js/importsections.js
+++ js/importsections.js
@@ -3,4 +3,5 @@
 $(function(){ 
     $("#header").load("./sections/header.html");
     $("#footer").load("./sections/footer.html"); 
+    $("#popular-itens").load("./sections/popular-itens.html"); 
 });

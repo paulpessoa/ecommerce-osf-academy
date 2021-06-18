825272f2214449e452ebddcc7911e537f132afeb
Paul Pessoa <paulmspessoa@gmail.com>
2021-06-18 01:40:14 -0300

    benefit content finish

--- js/importsections.js
+++ js/importsections.js
@@ -4,4 +4,5 @@ $(function(){
     $("#header").load("./sections/header.html");
     $("#footer").load("./sections/footer.html"); 
     $("#popular-itens").load("./sections/popular-itens.html"); 
+    $("#benefits").load("./sections/benefits.html"); 
 });

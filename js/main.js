dbaff331f1b732bb38fd10330f4652a360c101cd
Paul Pessoa <paulmspessoa@gmail.com>
2021-06-21 01:23:01 -0300

    studing loadmore ways

--- js/main.js
+++ js/main.js
@@ -12,3 +12,28 @@ function addCart() {
     numCart++;
     document.getElementById('add-cart-sum').textContent = numCart;
     }
+
+
+// load more button function
+
+$(function(){
+
+    let $data = $('#more-products');
+
+    $.ajax({
+        type: 'GET',
+        url: './data/product-list.json',
+        success: function(data){
+            $.each(data, function(i, data){
+                console.log('Sucess', data)
+                $data.append("<br><img src='" + data.cardimg + "' alt='Image not found'/><p>" + data.productname + '<br>'  + data.price + "</p>");
+            
+
+            });
+         
+        } 
+    });
+});
+
+
+

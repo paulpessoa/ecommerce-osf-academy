ccb52fc3cf3f375ad8ef8699cb500730b444aa59
Paul Pessoa <paulmspessoa@gmail.com>
2021-06-23 16:01:33 -0300

    thumb images product

--- js/main.js
+++ js/main.js
@@ -20,16 +20,11 @@ $(document).ready(function() {
     $('[data-target="#lightbox"]').on('click', function(event) {
         var $img = $(this).find('img'), 
             src = $img.attr('src'),
-            alt = $img.attr('alt'),
             css = {
                 'maxWidth': $(window).width() - 100,
                 'maxHeight': $(window).height() - 100
             };
-    
-        $lightbox.find('.close').addClass('hidden');
-        $lightbox.find('img').attr('src', src);
-        $lightbox.find('img').attr('alt', alt);
-        $lightbox.find('img').css(css);
+            $lightbox.find('img').attr('src', src);
     });
 });
 
